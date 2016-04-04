8b3d66bfa5e7293a7a90f06ddb5833a0c86b184b
Mollie Bootsma and Veronica Alley <Guest@epicodus-32.local>
2016-04-04 13:46:16 -0700

    start adding new listing function

--- app/models/category.js
+++ app/models/category.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  listings: DS.hasMany('listing', {async: true})
 });
--- app/models/listing.js
+++ app/models/listing.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr(),
+  price: DS.attr(),
+  contact: DS.attr(),
+  date: DS.attr(),
+  category: DS.belongsTo('category', {async: true})
 });
